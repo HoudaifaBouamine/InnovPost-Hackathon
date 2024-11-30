71ab82fe5c9ed044f8776d9ee4cf3ea4bf7aece8
Houdaifa Bouamine <houdaifa.bouamine@gmail.com>
2024-11-30 15:21:55 +0100

    add auth

--- server/Program.cs
+++ server/Program.cs
@@ -1,13 +1,47 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlite("Data Source=app.db"));
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<AppUser>()
+    .AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/", () => Results.Redirect("/swagger/index.html")).ExcludeFromDescription();
+var apiGroup = app.MapGroup("/api");
+
+var authGroup = apiGroup.MapGroup("/auth");
+
+authGroup.MapIdentityApi<AppUser>().WithTags("Auth");
+
+apiGroup.MapGet("/", () => Results.Ok("API root"))
+    .WithTags("General");
+
+app.MapGet("/", () => Results.Redirect("/swagger/index.html"))
+    .ExcludeFromDescription();
 
 app.Run("http://localhost:5000");
+
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) :
+        base(options)
+    { }
+}
+
+public class AppUser : IdentityUser
+{
+
+}
